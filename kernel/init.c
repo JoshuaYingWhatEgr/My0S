5bbc1e3a50af11588d537d593b1d2c26abf4e334
joshuayingwhat <joshuayingwhat@163.com>
2021-09-06 17:23:29 +0800

    添加计数器

--- kernel/init.c
+++ kernel/init.c
@@ -1,11 +1,21 @@
 #include "init.h"
 #include "print.h"
 #include "interrupt.h"
+#include "../device/timer.h"
 
 /**
  * 负责初始化所有模块
  * */
 void init_all() {
     put_str("init_all\n");
+
+    /**
+     * 初始化中断
+     */
     idt_init();
+
+    /**
+     * 启动计数器
+     */
+    timer_init();
 }
