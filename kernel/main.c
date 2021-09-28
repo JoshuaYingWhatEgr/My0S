c6f6027db16e2b764d37e6657a42baf7a08fb9ab
joshuayingwhat <joshuayingwhat@163.com>
2021-09-28 15:43:10 +0800

    提交代码

--- kernel/main.c
+++ kernel/main.c
@@ -5,7 +5,9 @@
 #include "../thread/thread.h"
 #include "interrupt.h"
 
-void k_thread_a(void*);
+void k_thread_a(void *);
+
+void k_thread_b(void *);
 
 /**
  * 程序运行开中断以及线程调度导致字符打印混乱
@@ -22,8 +24,8 @@ int main(void) {
 //    put_int((uint32_t) addr); //打印分配的虚拟页地址
 //    put_str("\n");
 
-    thread_start("k_thread_a",31,k_thread_a,"argA");
-    thread_start("k_thread_b",8, k_thread_b,"argB");
+    thread_start("k_thread_a", 31, k_thread_a, "argA");
+    thread_start("k_thread_b", 8, k_thread_b, "argB");
 
     intr_enable();//打开时钟中断
     while (1) {
@@ -34,20 +36,20 @@ int main(void) {
     return 0;
 }
 
-void k_thread_a(void* arg) {
-    char* par = arg;
+void k_thread_a(void *arg) {
+    char *par = arg;
 
-    while(1) {
+    while (1) {
         intr_disable(); //关中断 打印字符实现互斥
         put_str(par);
         intr_enable();//开中断
     }
 }
 
-void k_thread_b(void* arg) {
-    char* par = arg;
+void k_thread_b(void *arg) {
+    char *par = arg;
 
-    while(1) {
+    while (1) {
         intr_disable(); //关中断 打印字符实现互斥
         put_str(par);
         intr_enable();//开中断
